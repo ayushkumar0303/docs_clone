1d39f7b7a5e5cbbdc54f57f4ff7dca8b329db04f
Ayush Kumar <ayushk8295@gmail.com>
2025-05-13 11:14:42 +0530

    add editor toobar sections

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,5 +1,108 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { useEditorStore } from "@/store/use-editor-store";
+import { Separator } from "@/components/ui/separator";
+import {
+  BoldIcon,
+  ItalicIcon,
+  LucideIcon,
+  PrinterIcon,
+  Redo2Icon,
+  SpellCheckIcon,
+  Undo2Icon,
+} from "lucide-react";
 import React from "react";
 
+interface ToolbarButtonProps {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+}
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
+        isActive && "bg-neutral-200/80"
+      )}
+    >
+      <Icon className="size-4" />
+    </button>
+  );
+};
 export const Toolbar = () => {
-  return <div>toolbar</div>;
+  const { editor } = useEditorStore();
+  // console.log({ editor });
+
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => editor?.chain().focus().undo().run(),
+      },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().undo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+    ],
+  ];
+  return (
+    <div className="bg-[#f1f4f9] px-2.5 py-0.5 rounded-[25px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+      {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      <Separator orientation="vertical" className="h-6 bg-neutral-300" />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+    </div>
+  );
 };
